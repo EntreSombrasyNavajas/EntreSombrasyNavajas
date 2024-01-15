13ac4e26eef10c97a2edea4294bfa3047b4bc40f
Sebas-Ar <sebastian.arias@mercadolibre.com.co>
2024-01-15 03:37:18 -0500

    fix bug in mobile device

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -61,7 +61,7 @@ const MyBook = (props) => {
 
                 <div className="content">
                     <main>
-                        <HTMLFlipBook width={486} minWidth={300} height={680} showCover={true} size="stretch" ref={book} onFlip={onFlip}>
+                        <HTMLFlipBook width={486} minWidth={300} height={680} minHeight={420} showCover={true} size="stretch" ref={book} onFlip={onFlip}>
                             {
                                 Array(totalPage).fill(0).map((_, i) => (
                                     <div className="demoPage" key={i}>
